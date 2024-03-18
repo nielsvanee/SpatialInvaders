c99d368de931f05bcc8e7567c7742cfded81a72c
Niels van Ee <nielsvanee@hotmail.com>
2024-03-18 10:30:17 +0100

    Initial setup

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module SpatialInvaders {
+    requires hanyaeger;
+
+    exports org.example;
+}
---
+++ src/main/java/org/example/SpatialInvaders.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+/**
+ * Hello world!
+ *
+ */
+public class SpatialInvaders extends YaegerGame
+{
+    public static void main( String[] args ) {
+        launch(args);
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
